afcf68a476b0c37fed4c44d86598466cebfacee7
Aleksei <aleksei.karpukovich@student.ehu.lt>
2024-05-02 01:18:40 +0300

    feat: add alert

--- script.js
+++ script.js
@@ -1,86 +1,104 @@
-function loadUnsplashPhotos() {
-    var url = 'https://api.unsplash.com/photos/random?client_id=CokNVyEfqh41BLtagxfTIP08xIkTVkQZChTz6DX--9U&count=100'; 
+// function loadUnsplashPhotos() {
+//     var url = 'https://api.unsplash.com/photos/random?client_id=CokNVyEfqh41BLtagxfTIP08xIkTVkQZChTz6DX--9U&count=100'; 
 
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        var unsplashPhotos = document.getElementById('unsplash-photos');
+//     fetch(url)
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(data) {
+//         var unsplashPhotos = document.getElementById('unsplash-photos');
         
-        data.forEach(function(photo) {
-          var div = document.createElement('div');
-          div.className = 'img-style'; 
+//         data.forEach(function(photo) {
+//           var div = document.createElement('div');
+//           div.className = 'img-style'; 
 
-         var a = document.createElement('a');
-         a.href = '#'; 
+//          var a = document.createElement('a');
+//          a.href = '#'; 
 
-          var img = document.createElement('img');
-          img.src = photo.urls.regular;
+//           var img = document.createElement('img');
+//           img.src = photo.urls.regular;
 
-          var saveButton = document.createElement('button');
-          saveButton.innerText = 'Save';
-          saveButton.className = 'save-button';
+//           var saveButton = document.createElement('button');
+//           saveButton.innerText = 'Save';
+//           saveButton.className = 'save-button';
 
-          div.appendChild(saveButton);
-          a.appendChild(img)
-          div.appendChild(a); 
-          unsplashPhotos.appendChild(div); 
-        });
-      })
-      .catch(function(error) {
-        console.log('Error:', error);
-      });
-  }
+//           div.appendChild(saveButton);
+//           a.appendChild(img)
+//           div.appendChild(a); 
+//           unsplashPhotos.appendChild(div); 
+//         });
+//       })
+//       .catch(function(error) {
+//         console.log('Error:', error);
+//       });
+//   }
 
-  window.onload = loadUnsplashPhotos;
+//   window.onload = loadUnsplashPhotos;
 
 
 // for test
 
-// function loadUnsplashPhotos() {
-//     var url = 'assets/4Khw7f2hkI-enhance-8x.png';
-//     var unsplashPhotos = document.getElementById('unsplash-photos');
+function loadUnsplashPhotos() {
+    var url = 'assets/4Khw7f2hkI-enhance-8x.png';
+    var unsplashPhotos = document.getElementById('unsplash-photos');
   
-//     for (var i = 0; i < 36; i++) {
-//       var div = document.createElement('div');
-//       div.className = 'img-style';
+    for (var i = 0; i < 36; i++) {
+      var div = document.createElement('div');
+      div.className = 'img-style';
   
-//       var a = document.createElement('a');
-//       a.href = '#';
+      var a = document.createElement('a');
+      a.href = '#';
   
-//       var img = document.createElement('img');
-//       img.src = url;
+      var img = document.createElement('img');
+      img.src = url;
   
-//       var saveButton = document.createElement('button');
-//       saveButton.innerText = 'Save';
-//       saveButton.className = 'save-button';
+      var saveButton = document.createElement('button');
+      saveButton.innerText = 'Save';
+      saveButton.className = 'save-button';
   
-//       div.appendChild(saveButton);
-//       a.appendChild(img);
-//       div.appendChild(a);
-//       unsplashPhotos.appendChild(div);
-//     }
-// }
+      div.appendChild(saveButton);
+      a.appendChild(img);
+      div.appendChild(a);
+      unsplashPhotos.appendChild(div);
+    }
+}
   
-//     window.onload = loadUnsplashPhotos;
+    window.onload = loadUnsplashPhotos;
 
 
     document.addEventListener('DOMContentLoaded', () => {
       document.body.addEventListener('click', (event) => {
         if (event.target.matches('.save-button')) {
-          event.target.innerText = 'Saved';
-          event.target.style.backgroundColor = 'black';
-          event.target.style.color = 'azure';
-          event.target.style.borderColor = 'azure';
-          event.target.style.border = '1px solid';
-        }
-    
-        if (event.target.matches('.save-button') && getComputedStyle(event.target).backgroundColor === 'rgb(0, 0, 0)') {
-          event.target.innerText = 'Save';
-          event.target.style.backgroundColor = 'transparent';
-          event.target.style.color = 'black';
-          event.target.style.borderColor = '#1A1A1A';
+          if (getComputedStyle(event.target).backgroundColor === 'rgb(0, 0, 0)') {
+            event.target.innerText = 'Save';
+            event.target.style.backgroundColor = 'transparent';
+            event.target.style.color = 'black';
+            event.target.style.borderColor = '#1A1A1A';
+            
+            showAlert('Photo removed from the collection');
+          } else {
+            event.target.innerText = 'Saved';
+            event.target.style.backgroundColor = 'black';
+            event.target.style.color = 'azure';
+            event.target.style.borderColor = 'azure';
+            event.target.style.border = '1px solid';
+
+            showAlert('Photo added to the collection');
+          }
         }
       });
+    
+      function showAlert(message) {
+        const alertDiv = document.getElementById('alert');
+        alertDiv.innerHTML = '';
+        const messageParagraph = document.createElement('p');
+        messageParagraph.textContent = message;
+        alertDiv.appendChild(messageParagraph);
+        
+        alertDiv.style.display = 'flex';
+    
+        setTimeout(() => {
+          alertDiv.style.display = 'none';
+        }, 2500); 
+      }
     });
